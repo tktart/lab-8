59e5218255b452100254e0094d52b1361159a280
tktart <tonkow235@gmail.com>
2024-12-10 00:21:53 +0700

    oo

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -4,21 +4,39 @@
 using namespace std;
 
 //[Missing Code 1] Write definition of the function findGrade() here.
+char findGrade(double a){
 
+    if(a > 90){
+        return 'A';
+    }else if(a > 75){
+        return 'B';
+    }else if(a > 60){
+        return 'C';
+    }else if(a > 45){
+        return 'D';
+    }else{
+        return 'F';
+    }
+    
+}
 int main(){
 	//Input the number of students
 	int N,i = 0;
 	cout << "Enter the number of students: ";
 	cin >> N;
 	string name[N];
-	float score[N];	
+	float score[N];
+	cin.ignore();
 	
 	//Store names and scores of students into an array 
 	while(i < N){
 		cout << "Name of student " << i+1 << ": ";
-		cin.ignore();
+		getline(cin,name[i]);
+		
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
 		cout << "Score of student " << i+1 << ": ";
+		cin >> score[i];
+		cin.ignore();
 		//[Missing Code 3] Get score of the i-th students.
 		i++;
 	}
@@ -29,7 +47,7 @@ int main(){
 	cout << setw(25) << "Name" << setw(8) << "Score" << setw(8) << "Grade" << "\n";
 	cout << "---------------------------------------------\n";
 	while(i < N){
-		cout << setw(25) << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
+		cout << setw(25) << right << name[i] << setw(8) << score[i] << setw(8) << findGrade(score[i]) << "\n";
 		i++;
 	} 
 	cout << "---------------------------------------------\n";
